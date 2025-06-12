528dba7649c757c33e2b6cec86fb2f3243d6a047
Luis Felipe Bardi <lombardi131341@gmail.com>
2025-06-12 14:31:13 -0300

    feat: implement global authentication with public route decorator

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
 import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student";
 import { InvalidCredentialsError } from "@/domain/forum/application/use-cases/errors/invalid-credentials-error";
+import { Public } from "@/infra/auth/public";
 
 const authenticateBodySchema = z.object({
     email: z.string().email(),
@@ -12,6 +13,7 @@ const authenticateBodySchema = z.object({
 type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
 
 @Controller("/sessions")
+@Public()
 export class AuthenticateController {
     constructor(private authenticateStudent: AuthenticateStudentUseCase) { }
 
