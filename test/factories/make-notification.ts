562c9b246f980482c34efc69bb50bfc119dee2ca
Luis Felipe Bardi <lombardi131341@gmail.com>
2025-06-17 16:37:48 -0300

    feat: implement read notification functionality with controller and e2e tests

--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -1,6 +1,9 @@
 import { NotificationProps, Notification } from "@/domain/notification/enterprise/entities/notification";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { faker } from '@faker-js/faker';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaNotificationMapper } from "@/infra/database/prisma/mappers/prisma-notification-mapper";
 
 export function makeNotification(override: Partial<NotificationProps> = {}, id?: UniqueEntityId) {
     return Notification.create({
@@ -9,4 +12,19 @@ export function makeNotification(override: Partial<NotificationProps> = {}, id?:
         content: faker.lorem.sentence(10),
         ...override,
     }, id);
+}
+
+@Injectable()
+export class NotificationFactory {
+    constructor(private prisma: PrismaService) { }
+
+    async makePrismaNotification(data: Partial<NotificationProps> = {}): Promise<Notification> {
+        const notification = makeNotification(data);
+
+        await this.prisma.notification.create({
+            data: PrismaNotificationMapper.toPrisma(notification),
+        });
+
+        return notification;
+    }
 }
