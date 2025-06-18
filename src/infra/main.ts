a0080bb9e0718323ec41987cb3b4a3a30471b2ba
Luis Felipe Bardi <lombardi131341@gmail.com>
2025-06-18 10:46:12 -0300

    feat: add global exception filter for error handling and logging

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
+import { GlobalExceptionFilter } from './http/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   const envService = app.get(EnvService);
 
